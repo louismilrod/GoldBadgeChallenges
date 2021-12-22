abbaeffad7155583b8525c166f4c5670e94fe937
Louis Milrod <int4rweb@gmail.com>
2021-12-22 14:17:38 -0500

    organised code more

--- 02_Komodo_Claims/Program_UI.cs
+++ 02_Komodo_Claims/Program_UI.cs
@@ -63,33 +63,6 @@ namespace _02_Komodo_Claims
                 Console.WriteLine(claim.ToString());
             }
         }
-
-        private void WaitForKey()
-        {
-            Console.ReadKey();
-        }
-
-        private void Seed()
-        {            
-            DateTime dateOfIncident = new DateTime(2018, 04, 18);
-            DateTime dateOfClaim = new DateTime(2018, 04, 27);
-            Claim claimOne = new Claim(1, Claim.ClaimType.Car, "Car Accident on 465.", 400m, dateOfIncident, dateOfClaim);
-            _repo.EnterNewClaim(claimOne);
-
-            DateTime dateOfIncidentSeedTwo =new DateTime(2018, 04, 11);
-            DateTime dateOfClaimSeedTwo = new DateTime(2018,04,12);
-            Claim claimTwo = new Claim(2, Claim.ClaimType.Home, "House fire in kitchen", 4000m, dateOfIncidentSeedTwo, dateOfClaimSeedTwo);
-            _repo.EnterNewClaim(claimTwo);
-
-            DateTime dateOfIncidentSeedThree = new DateTime(2018, 04, 27);
-            DateTime dateOfClaimSeedThree = new DateTime(2018, 06, 01);
-            Claim claimThree = new Claim(3, Claim.ClaimType.Theft, "Stolen Pancakes", 4m, dateOfIncidentSeedThree, dateOfClaimSeedThree);
-            _repo.EnterNewClaim(claimThree);
-        }
-
-        //Do you want to deal with this claim now(y/n)? y
-        //When the agent presses 'y', the claim will be pulled off the top of the queue.If the agent presses 'n', it will go back to the main menu.
-
         public void DoYouWantClaimNow()
         {
             Console.Clear();
@@ -114,6 +87,10 @@ namespace _02_Komodo_Claims
             WaitForKey();           
         }
 
+        private void WaitForKey()
+        {
+            Console.ReadKey();
+        }
         public void EnterNewClaim()
         {
             Console.Clear();
@@ -141,6 +118,29 @@ namespace _02_Komodo_Claims
             
             _repo.EnterNewClaim(claim);
         }
+
+        private void Seed()
+        {            
+            DateTime dateOfIncident = new DateTime(2018, 04, 18);
+            DateTime dateOfClaim = new DateTime(2018, 04, 27);
+            Claim claimOne = new Claim(1, Claim.ClaimType.Car, "Car Accident on 465.", 400m, dateOfIncident, dateOfClaim);
+            _repo.EnterNewClaim(claimOne);
+
+            DateTime dateOfIncidentSeedTwo =new DateTime(2018, 04, 11);
+            DateTime dateOfClaimSeedTwo = new DateTime(2018,04,12);
+            Claim claimTwo = new Claim(2, Claim.ClaimType.Home, "House fire in kitchen", 4000m, dateOfIncidentSeedTwo, dateOfClaimSeedTwo);
+            _repo.EnterNewClaim(claimTwo);
+
+            DateTime dateOfIncidentSeedThree = new DateTime(2018, 04, 27);
+            DateTime dateOfClaimSeedThree = new DateTime(2018, 06, 01);
+            Claim claimThree = new Claim(3, Claim.ClaimType.Theft, "Stolen Pancakes", 4m, dateOfIncidentSeedThree, dateOfClaimSeedThree);
+            _repo.EnterNewClaim(claimThree);
+        }
+
+        //Do you want to deal with this claim now(y/n)? y
+        //When the agent presses 'y', the claim will be pulled off the top of the queue.If the agent presses 'n', it will go back to the main menu.
+
+
 
 
     }
